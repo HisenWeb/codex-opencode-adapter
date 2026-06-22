4591a731e421c37099a9d4feec515824ceed0dbc
hyson <499525549@qq.com>
2026-06-23 07:25:18 +0800

    Make Rust entrypoint logging initialization simpler

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,8 @@ use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env().add_directive("codex_opencode_adapter=info".parse()?))
-        .init();
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("codex_opencode_adapter=info"));
+    tracing_subscriber::fmt().with_env_filter(filter).init();
 
     let config = Config::parse();
     if config.local_token.as_deref() == Some(config.upstream_key.as_str()) {
